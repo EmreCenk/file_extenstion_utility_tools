a775f1d0ec3ed5f0a194c3a4dad8f2c2fcf6f9fb
Emre Cenk <emrecenk9@gmail.com>
2021-09-16 19:28:04 -0400

    documented and typehinted download_all function

--- extension_utils.py
+++ extension_utils.py
@@ -20,10 +20,18 @@ def change_extension(path: str, new_extension: str = ".png" ):
 
 
 
-def download_all(pic_list,
-                 folder_path = r"C:\Users\emrec\Desktop\Emre1\Programming\stuff\things\dp_",
-                 new_folder_name = "auto_download",
-                 create_new = True):
+def download_all(pic_list: str,
+                 folder_path: str = "",
+                 new_folder_name: str = "auto_download",
+                 create_new: bool = True):
+    """
+    Utility function to download everything in a list of image source urls
+    :param pic_list: list of picture source urls
+    :param folder_path: path tot the folder you would like to save
+    :param new_folder_name: name of the folder you would like to save this in
+    :param create_new: Whether you want to create a new folder or not (aka use a previous one)
+    :return:
+    """
     original = os.getcwd()
 
     os.chdir(folder_path)
